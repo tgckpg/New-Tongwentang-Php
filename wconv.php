8be3960f9451c0be702bd79b03f4011a25383338
斟酌 鵬兄 <tgckpg@gmail.com>
2016-08-26 18:17:46 +0800

    Wrong translation phases

--- wconv.php
+++ wconv.php
@@ -4,18 +4,18 @@ include "wtable.php";
 
 function ToHant($str) {
 	global $zh2Hant_phase1, $zh2Hant_phase2;
-	$phase1 = strtr($str, $zh2Hant_phase2);
+	$phase1 = strtr($str, $zh2Hant_phase1);
 	if(!$phase1)return $str;
 	
-	$phase2 = strtr($phase1, $zh2Hant_phase1);
+	$phase2 = strtr($phase1, $zh2Hant_phase2);
 	return ($phase2 ? $phase2 : $phase1);
 }
 
 function ToHans($str) {
 	global $zh2Hans_phase1, $zh2Hans_phase2;
-	$phase1 = strtr($str, $zh2Hans_phase2);
+	$phase1 = strtr($str, $zh2Hans_phase1);
 	if(!$phase1)return $str;
 	
-	$phase2 = strtr($phase1, $zh2Hans_phase1);
+	$phase2 = strtr($phase1, $zh2Hans_phase2);
 	return ($phase2 ? $phase2 : $phase1);
 }
